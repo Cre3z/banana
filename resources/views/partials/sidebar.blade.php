6c1a35495deab2404229f43783a9b13b7591112d
Cre3z <cnortje@hotmail.com>
2017-12-05 23:01:27 +0200

    updated partials

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,46 @@
+<div class="sidebar" data-color="purple" data-image="/img/sidebar-1.jpg">
+            <!--
+        Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
+
+        Tip 2: you can also add an image using data-image tag
+    -->
+            <div class="logo">
+                <a href="#" class="simple-text">
+                    Suzaan
+                </a>
+            </div>
+            <div class="sidebar-wrapper">
+                <ul class="nav">
+                    <li class="active">
+                        <a href="/home">
+                            <i class="material-icons">dashboard</i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/guests">
+                            <i class="material-icons">playlist_add_check</i>
+                            <p>To Do</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/guests">
+                            <i class="material-icons">attach_money</i>
+                            <p>Budget</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/guests">
+                            <i class="material-icons">person</i>
+                            <p>Guests</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/guests">
+                            <i class="material-icons">event_seat</i>
+                            <p>Seating</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
