5acb5183bdb2bd23a0b8018c34f6f5632213f384
Cre3z <cnortje@hotmail.com>
2018-05-01 21:12:20 +0200

    updated todos error

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
         $guest_total = Guest::count();
         $rsvp_invites = Guest::where('invited', true)->where('rsvp', 'yes')->count();
         $out_invites = Guest::where('invited', false)->count();
-        $todo = Todo::where('user', Auth::user()->email)->where('public', true)->first();
+        $todo = Todo::where('public', true)->first();
         return view('home', ['invites_sent'=>$invites_sent, 'guest_total'=>$guest_total, 'rsvp_invites'=>$rsvp_invites, 'out_invites'=>$out_invites, 'todo'=>$todo]);
     }
     
